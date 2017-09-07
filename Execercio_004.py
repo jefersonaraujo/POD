39a501816d98ba4fdec28ae506ef25640e921589
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-09-07 17:07:51 -0300

    Usando o 3n/2 comparações com o merge sort
    Usando o algoritmo merge, retorno na função ja com o vetor organizado o primeiro da função e ultimo, se o vetor estiver corretamente organizado, significa que o primeiro é o Menor e ultimo é o Maior.

--- Execercio_004.py
+++ Execercio_004.py
@@ -2,7 +2,40 @@
 # array de tamanho n usando menos que 3n/2 comparações. (Dica: Primeiro, construa um
 # grupo de mínimos candidatos e outro de máximos candidatos)
 
-
-array =[1,2,3,4]
-print (min(array))
-print (max(array))
+def mergeSort(alist):
+  #print("Splitting ",alist)
+  if len(alist)>1:
+    mid = len(alist)//2
+    lefthalf = alist[:mid]
+    righthalf = alist[mid:]
+    
+    mergeSort(lefthalf)
+    mergeSort(righthalf)
+    
+    i=0
+    j=0
+    k=0
+    while i < len(lefthalf) and j < len(righthalf):
+      if lefthalf[i] < righthalf[j]:
+        alist[k]=lefthalf[i]
+        i=i+1
+      else:
+        alist[k]=righthalf[j]
+        j=j+1
+      k=k+1
+      
+    while i < len(lefthalf):
+      alist[k]=lefthalf[i]
+      i=i+1
+      k=k+1
+      
+    while j < len(righthalf):
+      alist[k]=righthalf[j]
+      j=j+1
+      k=k+1
+  return (alist[0],alist[-1])
+  
+sequence = [1,-2,3,4,5,6,9]
+maxMin = mergeSort(sequence)
+print ("O Maior é : ", maxMin[1])
+print ("O Menor é : ", maxMin[0])
