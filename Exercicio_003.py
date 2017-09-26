a86b6f2e297c14c4eb09038893bee453b5599dc7
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-09-26 15:02:45 -0300

    Corrigido nomenclatura

---
+++ Exercicio_003.py
@@ -0,0 +1,50 @@
+# Escreva um programa em Python que recebe como argumento um polinômio em notação
+# algébrica e gera como saída a primeira derivada desse polinômio. Em seguida, calcule a
+# função T(n) (modelo simplificado) e faça o estudo do seu desempenho em uma tabela e um
+# gráfico.
+def poli(lista):
+                aux = ""
+                derivada = ""
+                polinomio = "f(x) = "
+                for i in range(len(lista)):
+                        exp = len(lista) -  1 - i
+
+                        if(exp == 1):
+                                aux = aux + " %dx + " %lista[i]
+                                derivada = derivada + " %d  " %(lista[i] * exp)
+
+                                
+                        elif(exp !=0 and (lista[i] !=0)):
+
+                                aux = aux + "%d" %lista[i] + "x ^ %d + " %exp
+                                if i == 0 :
+                                	expAux = exp -i -1
+                                else:
+                                	expAux = exp - i
+                                
+                                if expAux != 1 :
+                                	derivada = derivada + "%d" %(lista[i] * exp) + "x ^ %d + " %(expAux)
+                                else:
+                                	derivada = derivada + "%d" %(lista[i] * exp) + "x +"
+
+                        else:
+                        		if lista[i] !=0 :
+                        			aux = aux + "%d" %lista[i]
+                        			       
+                					
+                		#Tirar Primeira derivada
+
+                polinomio = polinomio + aux 
+                derivada = "der(x) = " + derivada
+                        
+                        
+                return polinomio,derivada
+
+
+
+
+sequence = [1,3,1,0,5,4]
+teste = poli(sequence)
+print (teste[0]) # poli
+print (teste[1]) # der
+
