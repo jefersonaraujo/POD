80227751dd04ff681f5800e2e6a5455b9c083512
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-11-19 17:48:34 -0300

    "Printando" os resultados a cada operação.

--- Bubble_Sort.py
+++ Bubble_Sort.py
@@ -1,14 +1,15 @@
 def burble_sort(A):
-    n = len(A)
-
+   
     for j in range(len(A)  -1):
         for i in range(len(A) - 1):
+                
                 if A[i] > A [i+1]:
                     tmp = A[i+1]
                     A[i+1] = A[i]
                     A[i]= tmp
-        print (A)
+        print (j," : ",A)
     return A
 
-sequence = [13,11,4,7,8,2]
+sequence = [9, 8, 2, 1, 6, 5]
+
 burble_sort(sequence)
