07d4e62c1b7e34203e1b558b267dd70b38f2b5c0
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-12-06 18:24:41 -0300

    Imprimindo lista de Combinações possiveis.

--- Exercicios_vp/VP3-Q1.py
+++ Exercicios_vp/VP3-Q1.py
@@ -3,6 +3,7 @@
 
 def compare(S,k):
     array = []
+    slots =[]
     # temp = -1
     for i in range(len(S)):
         for j in range(len(S)):
@@ -11,10 +12,13 @@ def compare(S,k):
             #print(S[i],S[j])
             if temp == k:
                 #print(temp)
-                print(S[i],"+ ",S[j]," = ",k)
-                #array.insert(S[i],S[j]+1)
+                #print(S[i],"+ ",S[j]," = ",k)
+                #slots.append([S[i],S[j]])
+                array.append([S[i],S[j]])
 
     return array
 
 sequence = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
+k=12
+print("As combinações de soma para :",k, "São:\n")
 print(compare(sequence,12))
