039a1e24d27e108218086c2f4bc6994242e05641
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-12-06 18:46:43 -0300

    Formatando Exibição

--- Exercicios_vp/VP3-Q2.py
+++ Exercicios_vp/VP3-Q2.py
@@ -1,8 +1,14 @@
-class atividade(object):
+class Activity(object):
     def __init__(self,id, inicial, fim):
         self.id = id
         self.si = inicial
         self.fi = fim
 
     def __str__(self):
-        return "(id : {}, duracao_inicial : {}, duracao_final : {})".format(self.id, self.si, self.fi)
+        return "(id : {}, hora_inicial : {}, hora_final : {})".format(self.id, self.si, self.fi)
+
+
+if __name__ == '__main__':
+    ativ = [Activity(1, 1,2),Activity(2, 2,4),Activity(3, 8,12)]
+    for act in ativ:
+        print(act)
