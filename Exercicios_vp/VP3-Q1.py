93850ec435ba8df3559574299b4a28bc454c57d5
Jeferson Araujo de Sousa <jefersonaraujo95@gmail.com>
2017-12-07 21:27:31 -0300

    Update VP3-Q1.py

--- Exercicios_vp/VP3-Q1.py
+++ Exercicios_vp/VP3-Q1.py
@@ -1,24 +1,17 @@
 #1) Suponha  que  você tenha um dada  sequência  de  n  elementos,  S,  contendo inteiros distintosque   estão   listados   em   ordem   crescente.   Dado   um   número   k, implementeum   algoritmorecursivo  para  encontrar  dois  inteiros  em  S, tais que  soma seja  igual  a  k,  se  existir tal par
 
+def soma(array,valor,inicial):
+    for i in range(len(array)):
+        if inicial == i:
+            continue
+        else:
+            temp = array[inicial] + array[i]
+            if temp == valor:
+                return [array[inicial],array[i]]
 
-def compare(S,k):
-    array = []
-    slots =[]
-    # temp = -1
-    for i in range(len(S)):
-        for j in range(len(S)):
-            #print(S[i],S[j]+1)
-            temp = S[i]+S[j]
-            #print(S[i],S[j])
-            if temp == k:
-                #print(temp)
-                #print(S[i],"+ ",S[j]," = ",k)
-                #slots.append([S[i],S[j]])
-                array.append([S[i],S[j]])
+    return tentativa(array,valor,inicial+1)
+    
+array = [0, 1, 2, 3, 4, 6, 7, 9,10,11, 12, 13,14,15]
+array = soma(array,29,0)
+print(array)
 
-    return array
-
-sequence = [1,2,3,4,5,6,7,8,9,10,11,12,13,14]
-k=12
-print("As combinações de soma para :",k, "São:\n")
-print(compare(sequence,12))
